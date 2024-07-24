3367fe627ce6cf8012a27e5c41cd41c9d887e6fe
KGH <gang2648@naver.com>
2024-07-24 16:27:24 +0900

    0724KGHH

--- backend/routes/mainrouter.js
+++ backend/routes/mainrouter.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const conn = require('../config/DB');
 
 
 // 메인 페이지
@@ -47,6 +48,68 @@ router.get("/boss_myPage", (req, res)=>{
 // 밸런스 매칭 기능 페이지 열어주기
 router.get("/bal_rate_tmmatch", (req, res) => {
     res.render("bal_rate_tmmatch");
+});
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 매치 페이지 
+router.get("/match", (req, res) => {
+    res.render("match", { idName: req.session.idName });
+});
+
+// 방 만들기 페이지
+router.get("/create_match", (req, res) => {
+    res.render("create_match", { idName: req.session.idName });
+});
+
+
+// 매칭방 입장 페이지 (3번 도전 성공)
+router.get('/match_room/:match_idx', (req, res) => {
+    const match_idx = req.params.match_idx;
+    console.log(`match_idx : ${match_idx}`);
+
+    let sql = "SELECT * FROM match_info1 WHERE match_idx = ?";
+    conn.query(sql, [match_idx], (err, result) => {
+        if (err) {
+            console.error(err);
+            res.send("매치 정보를 가져오는 중 오류가 발생했습니다.");
+        } else {
+            let match = result[0];
+            let join_users = match.join_user ? match.join_user.split(",").map(user => user.trim()).filter(user => user) : [];
+
+            if (join_users.length > 0) {
+                let placeholders = join_users.map(() => '?').join(',');
+                let user_sql = `SELECT user_id, user_rate FROM user_info WHERE user_id IN (${placeholders})`;
+
+                conn.query(user_sql, join_users, (err, users) => {
+                    if (err) {
+                        console.error(err);
+                        res.send("사용자 정보를 가져오는 중 오류가 발생했습니다.");
+                    } else {
+                        let user_info = {};
+                        users.forEach(user => {
+                            user_info[user.user_id] = user.user_rate;
+                        });
+                        const teamLeader = join_users[0]; // 방장 아이디 값 저장
+
+                        res.render('match_room', {
+                            match: match,
+                            idName: req.session.idName,
+                            join_users: join_users,
+                            user_info: user_info,
+                            teamLeader: teamLeader
+                        });
+                    }
+                });
+            } else {
+                res.render('match_room', {
+                    match: match,
+                    idName: req.session.idName,
+                    join_users: [],
+                    user_info: {}
+                });
+            }
+        }
+    });
 });
 
 module.exports = router;
