839dfae6d3a8337277958524c43aa90f187bb64b
YJH <yjy3070@naver.com>
2024-07-29 22:44:29 +0900

    0729_profileimg_yjh

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,9 @@ const fileStore = require("session-file-store")(session);
 const path = require("path"); // path 모듈 추가
 const conn = require("./config/DB.js");
 const WebSocket = require("ws");
+const uploadRouter = require('./routes/uploadRouter.js'); // uploadRouter 경로에 맞게 수정
+
+
 
 // 뷰 엔진 설정
 app.set("views", path.join(__dirname, "views"));
@@ -46,6 +49,12 @@ app.use("/user", userRouter);
 app.use("/reserv", reservRouter);
 app.use("/bal", balRouter);
 app.use("/manage", manageRouter);
+// 정적 파일 제공 설정 (업로드된 파일 접근 가능)
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+// 업로드 라우터 추가
+app.use('/api', uploadRouter);
+
 
 // 아이디 중복 확인 API
 app.post("/check-username", (req, res) => {
